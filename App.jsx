16d22c8637008293d723f17337d1ade3b87d8d8a
MalashreeH <127289640+MalashreeH@users.noreply.github.com>
2023-03-29 15:09:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import Avtar from './Avtar';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Avtar />
+//     </div>
+//   )
+// }
+
+// export default App
+import React from 'react'
+import Useeffect from './Useeffect';
+
+const App = () => {
+  return (
+   <Useeffect />
+  )
+}
+
+export default App
